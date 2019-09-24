0ef396b61e3790afb9216fe039ec9d90a98247a9
Ryan <rydowd@gmail.com>
2019-09-24 13:28:17 -0400

    added a function to populate the form

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ function qSelect(query) {
 function qSelectAll(query) {
     return document.querySelectorAll(query)
 }
+function populateForm(){
+    let form = new Form()
+    let inputs = qSelectAll("input")
+    for (let input of inputs){
+        let temp = new Field(input)
+        form.addField(temp)
+    }
+
+}
 
 class Form {
     constructor() {
@@ -13,23 +22,40 @@ class Form {
         this.fields.push(field)
     }
     clearErrors() {
-        
+        let errors = qSelectAll(".error")
+        for (let error in errors){
+            error.parentNode.removeChild(error)
+        }
     }
 }
 
 class Field {
-    constructor(field, type) {
+    constructor(field) {
         this.field = field
-        this.content = field.value
-        this.type = type
+        this.content = field.value.trim()
+        this.type = field.id
     }
-    validate(Field) {
-
+    validate() {
+        if(!this.content){
+            this.throwError()
+        }else{
+            this.showValid()
+        }    
+    }
+    throwError(){
+        this.field.classList.remove("input-valid")
+        this.field.classList.add("input-invalid")
+    }
+    showValid(){
+        this.field.classList.remove("input-invalid")
+        this.field.classList.add("input-valid")
     }
 }
 class CreditCard {
-    constructor(number) {
+    constructor(number, cvv, expiration) {
         this.number = number
+        this.cvv = cvv
+        this.expiration = expiration
     }
     validateCardNumber(number) {
         var regex = new RegExp("^[0-9]{16}$");
@@ -38,7 +64,18 @@ class CreditCard {
 
         return this.luhnCheck(number);
     }
-
+    validateExpiration(){
+        let regex = /^\d{2}\/\d{2}$/
+        console.log("expiration format test " + CanvasRenderingContext2D.test(this.expiration))
+        if (!regex.test(this.expiration)){
+            return false
+        }
+    }
+    validateCVV(){
+        let regex = /^\d{3$/
+        console.log("cvv test result " + regex.test(this.cvv))
+        return regex.text(this.cvv)
+    }
     luhnCheck(val) {
         var sum = 0;
         for (var i = 0; i < val.length; i++) {
